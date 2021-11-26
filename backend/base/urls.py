9fab21f5f05f390f9ff3fc04d30699cbbb5be31c
Sujit Packiaraj <sujit.pack@gmail.com>
2021-11-26 11:11:45 -0800

    Set up JWT tokens using Simple JWT

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
     path('', views.get_routes, name='routes'),
     path('products/', views.get_products, name='products'),
     path('products/<str:pk>/', views.get_product, name='product'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -12,14 +12,10 @@ def get_routes(request):
     routes = [
         '/api/products',
         '/api/products/create/',
-
         '/api/products/upload/',
-
         '/api/products/<id>/reviews/',
-
         '/api/products/top/'
         '/api/products/<id>/',
-
         '/api/products/delete/<id>/',
         '/api/products/update/<id>/',
     ]
