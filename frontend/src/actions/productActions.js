0e1e034f23aeb1017f8209ee0cbd33c49916e9cf
Sujit Packiaraj <sujit.pack@gmail.com>
2021-11-26 20:57:58 -0800

    Add user reducer and action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,9 +22,7 @@ export const listProducts = () => async (dispatch) => {
         dispatch({
             type: PRODUCT_LIST_FAIL,
             payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
+                error.response && error.response.data.detail ? error.response.data.detail : error.message,
         })
     }
 }
@@ -43,9 +41,7 @@ export const listProductDetails = (id) => async (dispatch) => {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
             payload:
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
+                error.response && error.response.data.detail ? error.response.data.detail : error.message,
         })
     }
 }
