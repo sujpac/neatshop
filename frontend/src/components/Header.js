f7b20b8935acac2cc9a7b1c07d555ee667d47a4d
Sujit Packiaraj <sujit.pack@gmail.com>
2021-11-30 17:16:16 -0800

    Add user and logout to navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,26 +1,49 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
+import { logout } from "../actions/userActions"
 
 function Header() {
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <LinkContainer to='/'>
+                    <LinkContainer to="/">
                         <Navbar.Brand>NeatShop</Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <i className="fas fa-shopping-cart"></i>Cart
+                                </Nav.Link>
+                            </LinkContainer>
 
-                            <LinkContainer to='/cart'>
-                                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                            </LinkContainer>
-                            
-                            <LinkContainer to='/login'>
-                                <Nav.Link href="/login"><i className="fas fa-user">Login</i></Nav.Link>
-                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id="username">
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link href="/login">
+                                        <i className="fas fa-user">Login</i>
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
